33bc0500958271acf46ca7d4abc00dcec4756b20
nhathuy110321 <nhathuy110321@gmail.com>
2023-05-26 11:04:08 +0700

    new

--- js/popup.js
+++
@@ -1,6 +0,0 @@
-function openLoginPopup() {
-    document.getElementById("loginPopup").style.display = "block";
-}
-function closeLoginPopup() {
-    document.getElementById("loginPopup").style.display = "none";
-}
